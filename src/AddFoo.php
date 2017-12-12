bf2b4b769a4ad2655c98495f3dc901673f45677d
Manuel Emilio Carpio <mectwork@gmail.com>
2017-12-12 14:29:00 -0500

    Removed not necessary services from controllers.

--- src/AddFoo.php
+++ src/AddFoo.php
@@ -2,38 +2,11 @@
 
 namespace Symsonte\JsApi;
 
-use Symsonte\Http\Server\Request\Resolution\NikicFastRouteFinder;
-use Symsonte\Service\Container;
-
 /**
  * @di\service()
  */
 class AddFoo
 {
-    /**
-     * @var NikicFastRouteFinder
-     */
-    private $controllerFinder;
-
-    /**
-     * @var Container
-     */
-    private $serviceContainer;
-
-    /**
-     * @param NikicFastRouteFinder $controllerFinder
-     * @param Container $serviceContainer
-     *
-     * @di\arguments({
-     *     serviceContainer: "@symsonte.service_kit.container"
-     * })
-     */
-    public function __construct(NikicFastRouteFinder $controllerFinder, Container $serviceContainer)
-    {
-        $this->controllerFinder = $controllerFinder;
-        $this->serviceContainer = $serviceContainer;
-    }
-
     /**
      * @http\resolution({method: "POST", path: "/add-foo"})
      *
@@ -47,13 +20,9 @@ class AddFoo
      * @throws BarException
      */
     public function add(
-        string $name, string $price
+        string $name,
+        string $price
     ) {
-        $controllers = $this->controllerFinder->all();
-
-        foreach ($controllers as $controller) {
-            list($controller, $method) = explode(':', $controller);
-            $controller = $this->serviceContainer->get($controller);
-        }
+        return "Foo added correctly";
     }
 }
--- src/CollectFoos.php
+++ src/CollectFoos.php
@@ -2,38 +2,11 @@
 
 namespace Symsonte\JsApi;
 
-use Symsonte\Http\Server\Request\Resolution\NikicFastRouteFinder;
-use Symsonte\Service\Container;
-
 /**
  * @di\service()
  */
 class CollectFoos
 {
-    /**
-     * @var NikicFastRouteFinder
-     */
-    private $controllerFinder;
-
-    /**
-     * @var Container
-     */
-    private $serviceContainer;
-
-    /**
-     * @param NikicFastRouteFinder $controllerFinder
-     * @param Container $serviceContainer
-     *
-     * @di\arguments({
-     *     serviceContainer: "@symsonte.service_kit.container"
-     * })
-     */
-    public function __construct(NikicFastRouteFinder $controllerFinder, Container $serviceContainer)
-    {
-        $this->controllerFinder = $controllerFinder;
-        $this->serviceContainer = $serviceContainer;
-    }
-
     /**
      * @http\resolution({method: "POST", path: "/collect-foos"})
      *
