91cbe4b5a50dea9a53fdfe6e4e7e528564a9b631
Manuel E Carpio <mectwork@gmail.com>
2017-12-12 22:18:11 -0500

    Changed array declaration to [].

--- src/GenerateCode.php
+++ src/GenerateCode.php
@@ -23,14 +23,16 @@ class GenerateCode
 
     /**
      * @param NikicFastRouteFinder $controllerFinder
-     * @param Container $serviceContainer
+     * @param Container            $serviceContainer
      *
      * @di\arguments({
      *     serviceContainer: "@symsonte.service_kit.container"
      * })
      */
-    public function __construct(NikicFastRouteFinder $controllerFinder, Container $serviceContainer)
-    {
+    public function __construct(
+        NikicFastRouteFinder $controllerFinder,
+        Container $serviceContainer
+    ) {
         $this->controllerFinder = $controllerFinder;
         $this->serviceContainer = $serviceContainer;
     }
@@ -41,23 +43,24 @@ class GenerateCode
     public function generate()
     {
         $controllers = $this->controllerFinder->all();
-        $fileString  = "";
+        $fileString = "";
 
         foreach ($controllers as $url => $controller) {
             list($controller, $method) = explode(':', $controller);
             $controller = $this->serviceContainer->get($controller);
 
             $reflector = new \ReflectionClass($controller);
-            $comment   = $reflector->getMethod($method)->getDocComment();
+            $comment = $reflector->getMethod($method)->getDocComment();
 
-            $factory  = DocBlockFactory::createInstance();
+            $factory = DocBlockFactory::createInstance();
             $docblock = $factory->create($comment);
 
             $exceptions = [];
             if ($docblock->hasTag('throws')) {
                 $tags = $docblock->getTagsByName('throws');
                 foreach ($tags as $index => $tag) {
-                    $exceptions[$index]['name'] = $tags[$index]->getType()->getFqsen()->getName();
+                    $exceptions[$index]['name'] = $tags[$index]->getType()
+                        ->getFqsen()->getName();
                     $exceptions[$index]['code'] = $this->generateExceptionCode(
                         $tags[$index]->getType()->getFqsen()->getName()
                     );
@@ -80,20 +83,22 @@ class GenerateCode
             }
 
             $mustache = new \Mustache_Engine(
-                array(
-                    'loader' => new \Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates'),
-                )
+                [
+                    'loader' => new \Mustache_Loader_FilesystemLoader(
+                        dirname(__FILE__).'/templates'
+                    ),
+                ]
             );
 
             $apiJs = $mustache->render(
                 'js-api',
-                array(
+                [
                     'method'     => $method,
                     'url'        => $url,
                     'parameters' => $parameters,
                     'data'       => $data,
                     'exceptions' => $exceptions,
-                )
+                ]
             );
 
             $fileString .= $apiJs;
@@ -111,7 +116,13 @@ class GenerateCode
      */
     private function generateExceptionCode(string $exception)
     {
-        $exceptionCode = strtolower(str_replace(' ', '-', trim(preg_replace("([A-Z])", " $0", $exception))));
+        $exceptionCode = strtolower(
+            str_replace(
+                ' ',
+                '-',
+                trim(preg_replace("([A-Z])", " $0", $exception))
+            )
+        );
 
         return $exceptionCode;
     }
